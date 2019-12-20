4f8a60badf9361ee7bc9d87ea0b683f2c123b345
David Erinayo <daviderinayo@gmail.com>
2019-12-20 03:33:28 +0100

    Stack overflow really helped here... lets hope it works

--- index.js
+++ index.js
@@ -25,9 +25,14 @@ app.use('/', pagesRoute);
 app.use('/', adminPagesRoute);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('static/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    // app.use(express.static('static/build'));
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    // })
+    const root = path.join(__dirname, 'client', 'build')
+    app.use(express.static(root));
+    app.get("*", (req, res) => {
+        res.sendFile('index.html', { root });
     })
 }
 const PORT = process.env.PORT || 4000;
